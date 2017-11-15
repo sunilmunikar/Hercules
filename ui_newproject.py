fbc85ac5ab621b528a8428d7e5cd30e160cd2bcc
AnujaDangol <dangolanuja@gmail.com>
2017-11-15 11:38:20 +0100

    buttons
    grouping and changind button for adding the event in window_newproject.py

--- ui_newproject.py
+++ ui_newproject.py
@@ -25,9 +25,43 @@ class NewProject(QtGui.QWidget):
 
         self.ProjectTitle = QtGui.QLineEdit()
 
+        '''making the buttons mutually exclusive and adding the event'''
         self.ProjectType = QtGui.QHBoxLayout()
-        self.ProjectType.addWidget(QtGui.QRadioButton("Archaeological Project"))
-        self.ProjectType.addWidget(QtGui.QRadioButton("Fieldwork Activity"))
+        self.b1 = QtGui.QRadioButton("Archaeological Project")
+        self.b1.setChecked(True)
+        #self.b1.stateChanged.connect(lambda: self.btnstate(self.b1))
+        self.ProjectType.addWidget(self.b1)
+
+        self.b2 = QtGui.QRadioButton("Fieldwork Activity")
+       # self.b2.toggled.connect(lambda: self.btnstate(self.b2))
+        self.ProjectType.addWidget(self.b2)
+
+        #putting the button in the group makes it mutually exclusive
+        self.ButtonGroup = QtGui.QButtonGroup()
+        self.ButtonGroup.addButton(self.b1, 1)
+        self.ButtonGroup.addButton(self.b2, 2)
+
+
+       # self.ProjectType.addWidget(QtGui.QRadioButton("Archaeological Project"))
+        #self.ProjectType.addWidget(QtGui.QRadioButton("Fieldwork Activity"))
+
+        self.ArchaeologicalProjectType = QtGui.QVBoxLayout()
+        self.ArchaeologicalProjectType.addWidget(QtGui.QCheckBox("Sagalassos"))
+        self.ArchaeologicalProjectType.addWidget(QtGui.QCheckBox("Other"))
+
+
+
+        self.FieldworkActivitySubType = QtGui.QComboBox()
+        self.FieldworkActivitySubType.setFixedWidth(200)
+        self.FieldworkActivitySubType.addItems(['Extensive Survey', 'Interdisciplinary Research', 'Intensive Survey',
+                                                'Excavation', 'Study', 'Building', 'Monument'])
+
+
+
+        self.FieldworkActivityType = QtGui.QVBoxLayout()
+        self.FieldworkActivityType.addWidget(QtGui.QCheckBox("Study Region"))
+        self.FieldworkActivityType.addWidget(QtGui.QCheckBox("Town"))
+        self.FieldworkActivityType.addWidget(self.FieldworkActivitySubType)
 
 
 
@@ -35,13 +69,11 @@ class NewProject(QtGui.QWidget):
 
         #self.ArchaeologicalProject = QtGui.QCheckBox("Archaeological Project")
         #self.FieldworkActivity = QtGui.QCheckBox("Fieldwork Activity")
-
-        self.ArchaeologicalProjectType = QtGui.QComboBox()
-        self.ArchaeologicalProjectType.setFixedWidth(200)
+        #
 
 
-        self.FieldworkActivityType = QtGui.QComboBox()
-        self.FieldworkActivityType.setFixedWidth(200)
+
+        #self.FieldworkActivityType.setFixedWidth(200)
 
 
         self.site = QtGui.QLineEdit()
@@ -63,33 +95,22 @@ class NewProject(QtGui.QWidget):
         self.gridlayout.addWidget(self.ProjectTitle, 0, 1, 1, 4)
 
         self.gridlayout.addWidget(self.ProjectLabel, 1, 0)
-        #self.gridlayout.setRowStretch(1, 1)
-        #self.gridlayout.setRowStretch(2, 1)
+        self.gridlayout.addLayout(self.ProjectType, 1, 1, 1, 3)
+        self.gridlayout.addLayout(self.ArchaeologicalProjectType, 3, 1, 1, 2)
+        self.gridlayout.addLayout(self.FieldworkActivityType, 3, 2, 1, 2)
 
+        self.gridlayout.addWidget(QtGui.QLabel('**************************************************************', self), 4, 1, 1, 7)
+        self.gridlayout.setRowStretch(5, 1)
 
+        self.gridlayout.addWidget(QtGui.QLabel('Site :', self), 6, 0)
+        self.gridlayout.addWidget(self.site, 6, 1)
 
+        self.gridlayout.addWidget(QtGui.QLabel('Date:', self), 7, 0)
+        self.gridlayout.addWidget(self.date, 7, 1)
 
-        self.gridlayout.addWidget(self.ArchaeologicalProject, 2, 1)
-
-        #self.gridlayout.setColumnStretch(1, 3)
-
-        self.gridlayout.addWidget(self.FieldworkActivity, 2, 3)
-        self.gridlayout.addWidget(self.ArchaeologicalProjectType, 3, 1, 1, 4)
-        #
-        #
-        self.gridlayout.addWidget(self.FieldworkActivityType, 3, 3, 1, 4)
-        #self.gridlayout.addWidget(QtGui.QLabel('', self), 4, 0)
-
-
-        self.gridlayout.addWidget(QtGui.QLabel('Site :', self), 5, 0)
-        self.gridlayout.addWidget(self.site, 5, 1)
-
-        self.gridlayout.addWidget(QtGui.QLabel('Date:', self), 6, 0)
-        self.gridlayout.addWidget(self.date, 6, 1)
-
-        self.gridlayout.addWidget(QtGui.QLabel('Study Area:', self), 7, 0)
-        self.gridlayout.addWidget(self.studyArea, 7, 1)
-        self.gridlayout.setRowStretch(8, 1)
+        self.gridlayout.addWidget(QtGui.QLabel('Study Area:', self), 8, 0)
+        self.gridlayout.addWidget(self.studyArea, 8, 1)
+        self.gridlayout.setRowStretch(10, 1)
 
 
 
