8caec619a3192e204d26282a18f7914f0b88b7e8
AnujaDangol <dangolanuja@gmail.com>
2017-11-16 12:45:04 +0100

    button clicked
    select button clicked text from multiple choices

--- widget_newproject.py
+++ widget_newproject.py
@@ -1,42 +1,49 @@
 import sys
-from PyQt4 import QtCore
 
-from PyQt4.QtGui import QListView, QWidget, QApplication, QStandardItemModel, QStandardItem, QLabel
+from PyQt4.QtCore import pyqtSlot
+from PyQt4.QtGui import QWidget, QApplication
+from PyQt4 import QtGui
 from newproject_repository import ExcavationRepository
-from  spatialiteConnection import SpatialiteDbConnect
 import ui_newproject
 
-
-class Form(QWidget, ui_newproject.NewProject):
+class NewprojectControl(QWidget, ui_newproject.NewProject):
     def __init__(self):
-        QWidget.__init__(self)
-        self.setupUi(self)
-        #self.setupUi(self)
+        # QWidget.__init__(self)
+        super(NewprojectControl, self).__init__()
+
+        self.setupUi()
 
         self.saveproject.clicked.connect(self.save_project)
-
-
-
         #self.saveproject.clicked.connect(self.save_project)
 
+        self.ButtonGroupProjectType.buttonClicked['QAbstractButton *'].connect(self.ButtonClickedText)
+        self.ButtonGroupSubType.buttonClicked['QAbstractButton *'].connect(self.ButtonClickedText)
+        #self.ButtonGroupFieldType.buttonClicked['QAbstractButton *'].connect(self.btngroup_clicked)
+
+
+        # self.ButtonGroup.buttonClicked['int'].connect(self.btngroup_clicked)
+
+        self.projectType = ""
+        self.projectSubtype = ""
 
     def save_project(self):
         self.insert_projectinfo()
 
-
-
     def insert_projectinfo(self):
-
         projectTitle = str(self.ProjectTitle.text())
-        print projectTitle
+        #print projectTitle
+
+        projectType = self.projectType
+        #print projectType
+
+        projectSubType = self.projectSubtype
+        #print projectSubType
+
+
 
         # ui.b1.stateChanged.connect(lambda: repo.btngroup(self.b1))
         # ui.b2.stateChanged.connect(lambda: repo.btngroup(self.b2))
         #projectType = self.ButtonGroup.buttonClicked.connect(lambda: self.buttonclickedText(self.ButtonGroup))
-        projectType = self.ButtonGroup.buttonClicked['QAbstractButton *'].connect(self.btngroup)
-
-
-        print projectType
 
         # ui.ProjectType = str(self.LocusTypeEntry.currentText())
         # print locus_type
@@ -54,13 +61,30 @@ class Form(QWidget, ui_newproject.NewProject):
         repo = ExcavationRepository()
         repo.insert_excavation(projectTitle)
 
-    def btngroup(self, btn):
-        print btn.text() + " is selected"
+    # def btngroup_clicked(self, button):
+    #     if button == self.ButtonGroupProjectType:
+    #     # print('{0} was clicked'.format(button_or_id.text()))
+    #
+    #         self.projectType = button.text()
+    #
+    #     elif button == self.ButtonGroupSubType:
+    #         self.projectSubtype = button.text()
+
+        #         print('"Id {}" was clicked'.format(button_or_id))
 
 
-    # def buttonclickedText(self, Button):
-    #     buttonText = Button.text()
-    #     print buttonText
+    # @pyqtSlot(QtGui.QAbstractButton)
+    # @pyqtSlot(int)
+    # def btngroup_clicked(self, button_or_id):
+    #     # print button_or_id.text() + " is selected"
+    #     # self.projectType = button_or_id.text()
+    #
+    #     if isinstance(button_or_id, QtGui.QAbstractButton):
+    #         print('{0} was clicked'.format(button_or_id.text()))
+    #     elif isinstance(button_or_id, int):
+    #         print('"Id {}" was clicked'.format(button_or_id))
+
+
     #     #return buttonText
     #
     # if b.text() == "Button1":
@@ -75,11 +99,31 @@ class Form(QWidget, ui_newproject.NewProject):
     #     else:
     #         print b.text() + " is deselected"
 
+    #
+    # def projecttypeClickedText(self, Button):
+    #
+    #     #if self.ButtonGroupProjectType.button(int) == True:
+    #         self.projectType = Button.text()
+    #     else:
+    #         print 'not selected '
+    #     print buttonText
+
+
+    def ButtonClickedText(self, Button):
+        if Button == self.ButtonGroupProjectType.checkedButton():
+            self.projectType = "{0}".format(Button.text(), Button)
+           # print "{0}".format(Button.text(), Button)
+
+        elif Button == self.ButtonGroupSubType.checkedButton():
+            self.projectSubtype = "{0}".format(Button.text(), Button)
+            #print "{0}".format(Button.text(), Button)
+
+
 
 
 def main():
     app = QApplication(sys.argv)
-    main = Form()
+    main = NewprojectControl()
 
     main.show()
     sys.exit(app.exec_())
